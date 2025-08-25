d64e3e8bb163c03063927ad1d3e31dc313ce06fe
Vouks <jotsaange@gmail.com>
2025-08-25 14:45:08 +0100

    autopull

--- auto-pull/index.js
+++ auto-pull/index.js
@@ -15,7 +15,7 @@ http.createServer((req, res) => {
             console.log("Webhook received:", new Date().toISOString());
 
             // Call your PowerShell script
-            exec('powershell -ExecutionPolicy Bypass -File "C:\\Users\\Administrator\\Desktop\\werewolf\\auto-pull"', (error, stdout, stderr) => {
+            exec('powershell -ExecutionPolicy Bypass -File "C:\\Users\\Administrator\\Desktop\\werewolf\\auto-pull\\update.ps1"', (error, stdout, stderr) => {
                 if (error) {
                     console.error(`Error: ${error.message}`);
                     return;
