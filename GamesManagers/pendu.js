fa198f253b65d6cb2120bb3afdd7550f5316a5dc
vouks <jotsaange@gmail.com>
2026-06-02 00:38:09 +0100

    uppp

--- GamesManagers/pendu.js
+++ GamesManagers/pendu.js
@@ -268,8 +268,8 @@ export class PenduManager {
             return { jid: p.jid, correctCount, incorrectCount }
         })
 
-        const percentageLeft = 100 - (game.wrongLetters.length * 17.5)
-        const paidMise = game.mise * (percentageLeft / 100)
+        //const percentageLeft = 100 - (game.wrongLetters.length * 17.5)
+        const paidMise = game.mise * (90 / 100)
         const totalPoints = playerScores.reduce((sum, p) => sum += (p.correctCount - p.incorrectCount), 0)
 
         if (game.gameType === 2) {
